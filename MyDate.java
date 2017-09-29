0255e4e1a4418e3488723d114199200f1160e51e
JGPNS <PlusNinetySix@users.noreply.github.com>
2017-09-29 13:37:31 -0500

    added this class

---
+++ MyDate.java
@@ -0,0 +1,52 @@
+package g;
+
+public class MyDate implements DateInterface {
+
+	public int getDay() 
+	{
+		return 0;
+	}
+	
+	public int getDow() 
+	{
+		
+		return 0;
+	}
+	
+	public int getMonth() 
+	{
+		
+		return 0;
+	}
+	
+	public int getYear() 
+	{
+		
+		return 0;
+	}
+
+	public void set(int m, int d, int y, int dow) 
+	{
+		
+
+	}
+	
+	public void tomorrow() 
+	{
+		
+
+	}
+
+	public void today() 
+	{
+		
+
+	}
+	
+	public void yesterday() 
+	{
+		
+
+	}
+
+}
