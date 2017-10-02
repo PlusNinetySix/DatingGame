1a09e15016c64c8a2c432d37062b7de18d28a97b
JGPNS <PlusNinetySix@users.noreply.github.com>
2017-10-02 12:46:15 -0500

    updated this

--- MyDate.java
+++ MyDate.java
@@ -2,40 +2,59 @@ package g;
 
 public class MyDate implements DateInterface
 {
+	static int yr;
+	static int mo;
+	static int day;
+	static int wk;
+	static String[] days = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+	static String[] months = {"January","February","March","April","May","June","July","August","September","October","November","December"};
 
 	public int getDay() 
 	{
-		return 0;
+		return day;
 	}
 	
 	public int getDow() 
 	{
-		
-		return 0;
+		return wk;
 	}
 	
 	public int getMonth() 
 	{
-		
-		return 0;
+		return mo;
 	}
 	
 	public int getYear() 
 	{
-		
-		return 0;
+		return yr;
 	}
 
 	public void set(int m, int d, int y, int dow) 
 	{
-		
-
+		mo = m;
+		day=d;
+		yr=y;
+		wk=dow;
 	}
 	
 	public void tomorrow() 
 	{
-		
-
+		day++;
+		wk++;
+		if(day>31) 
+		{
+			mo++;
+			day=1;
+		}
+		if(mo>11)
+		{
+			yr++;
+			mo=0;
+		}
+		if(wk>6)
+		{
+			wk=0;
+		}
 	}
 
 	public void today() 
@@ -46,8 +65,12 @@ public class MyDate implements DateInterface
 	
 	public void yesterday() 
 	{
-		
 
+	}
+	
+	public String toString()
+	{
+		return days[wk] + " " + months[mo] + " " + day + ", " + yr;
 	}
 
 }
